bfe4c57febd65cf0d37f649313089e4c7dedae2d
yuto0623 <yuto.ryr0623@gmail.com>
2025-03-24 13:37:16 +0900

    Update Calendar component styles for improved button hover effects and today flag display

--- components/ui/calendar.tsx
+++ components/ui/calendar.tsx
@@ -58,14 +58,14 @@ function Calendar({
 					"h-9 w-9 text-center rounded-md text-sm p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
 				[UI.DayButton]: cn(
 					buttonVariants({ variant: "ghost" }),
-					"h-9 w-9 p-0 font-normal aria-selected:opacity-100 hover:bg-primary hover:text-primary-foreground",
+					"h-9 w-9 p-0 font-normal aria-selected:opacity-100 hover:bg-primary dark:hover:bg-white hover:text-primary-foreground",
 				),
 				[SelectionState.range_end]: "day-range-end",
 				[SelectionState.selected]:
 					"bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
 				[SelectionState.range_middle]:
 					"aria-selected:bg-accent aria-selected:text-accent-foreground",
-				[DayFlag.today]: "bg-accent text-accent-foreground",
+				[DayFlag.today]: "bg-accent text-accent-foreground :",
 				[DayFlag.outside]:
 					"day-outside text-muted-foreground opacity-50 aria-selected:bg-accent/50 aria-selected:text-muted-foreground aria-selected:opacity-30",
 				[DayFlag.disabled]: "text-muted-foreground opacity-50",
